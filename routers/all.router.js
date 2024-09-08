b625b6b7cd621ff01972f2e3585f9def20774e46
= <=>
2024-09-08 14:57:41 +0100

    updated

--- routers/all.router.js
+++ routers/all.router.js
@@ -41,7 +41,7 @@ module.exports = app => {
     app.use((req, res, next) => {
       res.header('Access-Control-Allow-Credentials', true)
       // res.header('Access-Control-Allow-Origin', getSiteBaseURL())
-      //res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://studentshare-server.vercel.app'])
+      res.header('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://studentshare-server.vercel.app'])
       res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
       next()
     })
